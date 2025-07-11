5527a45e0fd6d61e4cd5bcdb60eaf94b63927581
longdo <dolong2310@gmail.com>
2025-07-12 01:57:31 +0700

    add: payload integration

---
+++ src/app/(app)/(home)/page.tsx
@@ -0,0 +1,16 @@
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
+
+const Home = async () => {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+
+  const data = await payload.find({
+    collection: "categories",
+  });
+
+  return <div className="p-4">{JSON.stringify(data, null, 2)}</div>;
+};
+
+export default Home;
