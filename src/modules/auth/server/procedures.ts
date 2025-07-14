c6e285a64e1104d084d0f1a718015aeb4b489111
longdo <dolong2310@gmail.com>
2025-07-14 19:34:19 +0700

    add: authentication & sign in, sign up

---
+++ src/modules/auth/server/procedures.ts
@@ -0,0 +1,111 @@
+import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { TRPCError } from "@trpc/server";
+import { cookies as getCookies, headers as getHeaders } from "next/headers";
+import { AUTH_COOKIE } from "../constants";
+import { loginSchema, registerSchema } from "../schemas";
+
+export const authRouter = createTRPCRouter({
+  session: baseProcedure.query(async ({ ctx }) => {
+    const headers = await getHeaders();
+    const session = await ctx.db.auth({ headers });
+    return session;
+  }),
+  register: baseProcedure
+    .input(registerSchema)
+    .mutation(async ({ input, ctx }) => {
+      // Check if user already exists
+      const existingData = await ctx.db.find({
+        collection: "users",
+        limit: 1,
+        where: {
+          username: {
+            equals: input.username,
+          },
+        },
+      });
+      const existingUser = existingData.docs[0];
+
+      if (existingUser) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Username already taken",
+        });
+      }
+
+      // register
+      await ctx.db.create({
+        collection: "users",
+        data: {
+          username: input.username,
+          email: input.email,
+          password: input.password,
+        },
+      });
+
+      // login
+      const data = await ctx.db.login({
+        collection: "users",
+        data: {
+          email: input.email,
+          password: input.password,
+        },
+      });
+
+      if (!data.token) {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+          message: "Invalid email or password",
+        });
+      }
+
+      const cookies = await getCookies();
+      cookies.set({
+        name: AUTH_COOKIE,
+        value: data.token,
+        httpOnly: true,
+        path: "/",
+        // TODO: Ensure cross-domain cookie sharing
+        // sameSite: "none",
+        // domain: "",
+        // nexstp.com -> initial cookie
+        // longdoo.nexstp.com -> cookie does not exist here
+      });
+
+      return data;
+    }),
+  login: baseProcedure.input(loginSchema).mutation(async ({ input, ctx }) => {
+    const data = await ctx.db.login({
+      collection: "users",
+      data: {
+        email: input.email,
+        password: input.password,
+      },
+    });
+
+    if (!data.token) {
+      throw new TRPCError({
+        code: "UNAUTHORIZED",
+        message: "Invalid email or password",
+      });
+    }
+
+    const cookies = await getCookies();
+    cookies.set({
+      name: AUTH_COOKIE,
+      value: data.token,
+      httpOnly: true,
+      path: "/",
+      // TODO: Ensure cross-domain cookie sharing
+      // sameSite: "none",
+      // domain: "",
+      // nexstp.com -> initial cookie
+      // longdoo.nexstp.com -> cookie does not exist here
+    });
+
+    return data;
+  }),
+  logout: baseProcedure.mutation(async () => {
+    const cookies = await getCookies();
+    cookies.delete(AUTH_COOKIE);
+  }),
+});
