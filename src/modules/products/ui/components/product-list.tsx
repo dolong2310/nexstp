8397db7427711ef33cd3c203ad139a7f1f7ae879
longdo <dolong2310@gmail.com>
2025-07-16 15:42:12 +0700

    add: create product card ui & update: split product list view for subcategory & update: product list ui

--- src/modules/products/ui/components/product-list.tsx
+++ src/modules/products/ui/components/product-list.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { DEFAULT_LIMIT } from "@/constants";
 import { useTRPC } from "@/trpc/client";
-import { useSuspenseQuery } from "@tanstack/react-query";
-import React from "react";
+import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
 import useProductFilter from "../../hooks/use-product-filter";
+import ProductCard, { ProductCardSkeleton } from "./product-card";
+import { InboxIcon } from "lucide-react";
 
 type Props = {
   category?: string | null;
@@ -12,28 +15,79 @@ type Props = {
 const ProductList = ({ category }: Props) => {
   const [filters] = useProductFilter();
   const trpc = useTRPC();
-  const { data } = useSuspenseQuery(
-    trpc.products.getMany.queryOptions({
-      category,
-      ...filters,
-    })
-  );
+  const { data, hasNextPage, isFetchingNextPage, fetchNextPage } =
+    useSuspenseInfiniteQuery(
+      trpc.products.getMany.infiniteQueryOptions(
+        {
+          ...filters,
+          category,
+          limit: DEFAULT_LIMIT,
+        },
+        {
+          getNextPageParam: (lastPage) => {
+            return lastPage.docs.length > 0 ? lastPage.nextPage : undefined;
+          },
+        }
+      )
+    );
+
+  if (data.pages?.[0]?.docs.length === 0) {
+    return <ProductListEmpty />;
+  }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
-      {data?.docs.map((product) => (
-        <div key={product.id} className="border rounded-md bg-white p-4">
-          <h2 className="text-xl font-medium">{product.name}</h2>
-          <p>{product.price}</p>
-        </div>
-      ))}
-      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+        {data?.pages.flatMap((page) => {
+          return page.docs.map((product) => (
+            <ProductCard
+              key={product.id}
+              id={product.id}
+              name={product.name}
+              imageUrl={product.image?.url}
+              authorUsername={"john doe"}
+              authorImageUrl={undefined}
+              reviewRating={5}
+              reviewCount={3}
+              price={product.price}
+            />
+          ));
+        })}
+      </div>
+
+      <div className="flex justify-center pt-8">
+        {hasNextPage && (
+          <Button
+            className="text-base font-medium bg-white disabled:opacity-50"
+            variant="elevated"
+            disabled={isFetchingNextPage}
+            onClick={() => fetchNextPage()}
+          >
+            Load more
+          </Button>
+        )}
+      </div>
+    </>
+  );
+};
+
+const ProductListEmpty = () => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-y-4 w-full rounded-lg bg-white border border-black border-dashed p-8">
+      <InboxIcon />
+      <p className="text-base font-medium">No products found</p>
     </div>
   );
 };
 
 export const ProductListSkeleton = () => {
-  return <div>Loading products...</div>;
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+      {Array.from({ length: DEFAULT_LIMIT }).map((_, index) => (
+        <ProductCardSkeleton key={index} />
+      ))}
+    </div>
+  );
 };
 
 export default ProductList;
---
+++ src/modules/products/views/product-list-view.tsx
@@ -0,0 +1,35 @@
+import { Suspense } from "react";
+import ProductFilters from "../ui/components/product-filters";
+import ProductList, {
+  ProductListSkeleton,
+} from "../ui/components/product-list";
+import ProductSort from "../ui/components/product-sort";
+
+type Props = {
+  category?: string | null;
+};
+
+const ProductListView = ({ category }: Props) => {
+  return (
+    <div className="flex flex-col gap-4 px-4 lg:px-12 py-8">
+      <div className="flex flex-col lg:flex-row lg:items-center justify-between gap-y-2 lg:gap-y-0">
+        <p className="text-2xl font-medium">Curated for you</p>
+        <ProductSort />
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-6 xl:grid-cols-8 gap-y-6 gap-x-12">
+        <div className="lg:col-span-2 xl:col-span-2">
+          <ProductFilters />
+        </div>
+
+        <div className="lg:col-span-4 xl:col-span-6">
+          <Suspense fallback={<ProductListSkeleton />}>
+            <ProductList category={category} />
+          </Suspense>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductListView;
