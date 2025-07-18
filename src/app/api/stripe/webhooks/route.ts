09b1c06e6c45d19acf8637fa5897f03bf491f577
longdo <dolong2310@gmail.com>
2025-07-19 01:06:49 +0700

    add: setup stripe connect in dashboard & add: create verify checkout prodedure to verify tenant & add: create button verify from CMS & update: register procedure to assign accountId to tenant & update: purchase procedure to take fee percentage

--- src/app/api/stripe/webhooks/route.ts
+++ src/app/api/stripe/webhooks/route.ts
@@ -31,7 +31,10 @@ export async function POST(request: Request) {
 
   console.log(`✅ Success: ${event.id} - ${event.type}`);
 
-  const permittedEvents: string[] = ["checkout.session.completed"];
+  const permittedEvents: string[] = [
+    "checkout.session.completed",
+    "account.updated",
+  ];
 
   const payload = await getPayload({ config });
 
@@ -60,6 +63,9 @@ export async function POST(request: Request) {
             data.id,
             {
               expand: ["line_items.data.price.product"],
+            },
+            {
+              stripeAccount: event.account,
             }
           );
 
@@ -78,17 +84,34 @@ export async function POST(request: Request) {
               collection: "orders",
               data: {
                 stripeCheckoutSessionId: data.id,
+                stripeAccountId: event.account,
                 user: user.id,
                 product: item.price.product.metadata.id,
                 name: item.price.product.metadata.name,
               },
             });
           }
-          break;
 
+          break;
+        case "account.updated":
+          data = event.data.object as Stripe.Account;
+          console.log(123123);
+
+          await payload.update({
+            collection: "tenants",
+            where: {
+              stripeAccountId: {
+                equals: data.id,
+              },
+            },
+            data: {
+              stripeDetailsSubmitted: data.details_submitted,
+            },
+          });
+
+          break;
         default:
           throw new Error(`Unhandled event type: ${event.type}`);
-          break;
       }
     } catch (error) {
       console.error(`⚠️ Error processing webhook: ${error}`);
