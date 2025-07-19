3bf6253892a4e42f0b3672d1f24fef1f76c81a04
longdo <dolong2310@gmail.com>
2025-07-19 17:21:19 +0700

    update: build for deployment

--- src/app/(app)/(auth)/sign-up/page.tsx
+++ src/app/(app)/(auth)/sign-up/page.tsx
@@ -2,9 +2,8 @@ import SignUpView from "@/modules/auth/ui/views/sign-up-view";
 // import { caller } from "@/trpc/server";
 // import { redirect } from "next/navigation";
 
-type Props = {};
 
-const SignUpPage = (props: Props) => {
+const SignUpPage = () => {
   // const session = await caller.auth.session();
 
   // if (session.user) {
