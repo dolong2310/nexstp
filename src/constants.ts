c29f5e76d4c023984056bae19381753d128d0d53
longdo <dolong2310@gmail.com>
2025-07-26 15:56:38 +0700

    add: create top loader

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,19 @@ export const DEFAULT_LIMIT = 8;
 export const PLATFORM_FEE_PERCENTAGE = 10;
 export const IS_DEVELOPMENT = process.env.NODE_ENV === "development";
 export const IS_PRODUCTION = process.env.NODE_ENV === "production";
+
+export const defaultConfigTopLoader = {
+  color: "#fb64b6",
+  initialPosition: 0.08,
+  crawlSpeed: 200,
+  height: 3,
+  crawl: true,
+  showSpinner: false,
+  easing: "ease",
+  speed: 200,
+  shadow: "none", // "0 0 10px #fb64b6,0 0 5px #fb64b6",
+  template:
+    '<div class="bar" role="bar"><div class="peg"></div></div> <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>',
+  zIndex: 1600,
+  showAtBottom: false,
+};
